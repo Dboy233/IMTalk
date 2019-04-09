140c525234e904d4a38284df44ccef37cd8eecc0
DJC <37604230+Dboy233@users.noreply.github.com>
2019-04-09 16:25:31 +0800

    注册界面的mvp协议

---
+++ app/src/main/java/com/djc/imtalk/contract/RegisterContract.kt
@@ -0,0 +1,21 @@
+package com.djc.imtalk.contract
+
+/**
+ *@author ： created by dujiangchuan
+ * 时间    ：2019/4/9 16
+ * 邮箱    ：894230813@qq.com
+ */
+interface RegisterContract {
+    interface Presenter : BasePresenter {
+        fun register(userName: String, password: String, confirmPassword: String)
+    }
+
+    interface View {
+        fun onUserNameError()
+        fun onPasswordError()
+        fun onConFirmPasswordError()
+        fun onStartRegister()
+        fun onRegisterSuccess()
+        fun onRegisterFailed()
+    }
+}
