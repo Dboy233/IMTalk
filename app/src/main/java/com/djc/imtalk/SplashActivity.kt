4d6eafc64359049d26c58eb1e6c03bd1c925ab4a
DJC <37604230+Dboy233@users.noreply.github.com>
2019-04-09 13:04:43 +0800

    Splash界面实现

---
+++ app/src/main/java/com/djc/imtalk/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.djc.imtalk
+
+/**
+ *@author ： created by dujiangchuan
+ * 时间    ：2019/4/9 11
+ * 邮箱    ：894230813@qq.com
+ */
+class SplashActivity : BaseActivity() {
+    override fun getLayoutResId(): Int = R.layout.activity_splash
+
+}
