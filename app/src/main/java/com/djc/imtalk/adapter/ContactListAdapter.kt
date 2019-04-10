c65a895905c1d2dd1fad7c6c2cdcadb24b7d21ad
DJC <37604230+Dboy233@users.noreply.github.com>
2019-04-10 20:59:50 +0800

    联系人列表 删除好友对话框实现

--- app/src/main/java/com/djc/imtalk/adapter/ContactListAdapter.kt
+++ app/src/main/java/com/djc/imtalk/adapter/ContactListAdapter.kt
@@ -1,9 +1,11 @@
 package com.djc.imtalk.adapter
 
 import android.content.Context
+import android.support.v7.app.AlertDialog
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
+import com.djc.imtalk.R
 import com.djc.imtalk.data.ContactListItem
 import com.djc.imtalk.ui.activity.ChatActivity
 import com.djc.imtalk.widget.ContactListItemView
@@ -30,7 +32,29 @@ class ContactListAdapter(
         val contactListItemView = holder.itemView as ContactListItemView
         contactListItemView.bindView(contactListItems[position])
         val userName = contactListItems[position].userName
+        //点击跳转到聊天界面
         contactListItemView.setOnClickListener { context.startActivity<ChatActivity>("userName" to userName) }
+        //
+        contactListItemView.setOnLongClickListener {
+            var delete_confirm = String.format(context.getString(R.string.delete_frient_confirm), userName)
+            AlertDialog.Builder(context).apply {
+                setTitle(R.string.delete_friend)
+                setMessage(delete_confirm)
+                setNegativeButton(context.getString(R.string.cancel), null)
+                setPositiveButton(
+                    context.getString(R.string.confirm)
+                ) { dialogInterface, i ->
+                    deleteFriend(userName)
+                }
+                show()
+            }
+
+            true
+        }
+    }
+
+    fun deleteFriend(userName: String) {
+
     }
 
     class ContactListItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
