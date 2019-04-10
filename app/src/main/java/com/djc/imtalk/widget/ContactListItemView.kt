32878df79b2e17c4a4a7f10828062dafcc5c6941
DJC <37604230+Dboy233@users.noreply.github.com>
2019-04-10 19:34:09 +0800

    联系人列表布局完成

---
+++ app/src/main/java/com/djc/imtalk/widget/ContactListItemView.kt
@@ -0,0 +1,20 @@
+package com.djc.imtalk.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.RelativeLayout
+import com.djc.imtalk.R
+
+/**
+ *@author ： created by dujiangchuan
+ * 时间    ：2019/4/10 19
+ * 邮箱    ：894230813@qq.com
+ */
+class ContactListItemView(context: Context?, attrs: AttributeSet? = null) : RelativeLayout(context, attrs) {
+
+    init {
+        View.inflate(context, R.layout.view_contact_item, this)
+    }
+
+}
