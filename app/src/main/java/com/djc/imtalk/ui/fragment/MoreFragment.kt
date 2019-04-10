9db5bb10f54df1e1e4063203f246c36b97b7b3ba
DJC <37604230+Dboy233@users.noreply.github.com>
2019-04-10 19:10:41 +0800

    推出功能实现

--- app/src/main/java/com/djc/imtalk/ui/fragment/MoreFragment.kt
+++ app/src/main/java/com/djc/imtalk/ui/fragment/MoreFragment.kt
@@ -1,9 +1,14 @@
 package com.djc.imtalk.ui.fragment
 
 import com.djc.imtalk.R
+import com.djc.imtalk.adapter.EMCallBackAdapter
+import com.djc.imtalk.ui.activity.LoginActivity
 import com.hyphenate.chat.EMClient
 import kotlinx.android.synthetic.main.fragment_more.*
 import kotlinx.android.synthetic.main.header.*
+import org.jetbrains.anko.runOnUiThread
+import org.jetbrains.anko.support.v4.startActivity
+import org.jetbrains.anko.toast
 
 /**
  *@author ： created by dujiangchuan
@@ -19,5 +24,27 @@ class MoreFragment : BaseFragment() {
         //登录按钮文字格式化
         val logoutString = String.format(getString(R.string.logout), EMClient.getInstance().currentUser)
         bt_logout.text = logoutString
+
+        //退登录功能
+        bt_logout.setOnClickListener { logout() }
+
+    }
+
+    private fun logout() {
+
+        EMClient.getInstance().logout(true, object : EMCallBackAdapter() {
+            override fun onSuccess() {
+                //回到登录界面
+                context!!.runOnUiThread { toast(getString(R.string.logout_success)) }
+                startActivity<LoginActivity>()
+                activity!!.finish()
+            }
+
+            override fun onError(p0: Int, p1: String?) {
+                //提醒推出失败
+                context!!.runOnUiThread { toast(getString(R.string.logout_failed)) }
+            }
+        })
+
     }
 }
