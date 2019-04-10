5695d1b793063b4872ad16fbbe91e060fc375447
DJC <37604230+Dboy233@users.noreply.github.com>
2019-04-10 19:19:00 +0800

    联系人界面初始化

--- app/src/main/java/com/djc/imtalk/ui/fragment/ContactFragment.kt
+++ app/src/main/java/com/djc/imtalk/ui/fragment/ContactFragment.kt
@@ -1,12 +1,25 @@
 package com.djc.imtalk.ui.fragment
 
+import android.view.View
 import com.djc.imtalk.R
+import kotlinx.android.synthetic.main.fragment_contact.*
+import kotlinx.android.synthetic.main.header.*
 
 /**
  *@author ： created by dujiangchuan
  * 时间    ：2019/4/9 19
  * 邮箱    ：894230813@qq.com
  */
-class ContactFragment :BaseFragment() {
-    override fun getLayoutResId(): Int  = R.layout.fragment_contact
+class ContactFragment : BaseFragment() {
+    override fun getLayoutResId(): Int = R.layout.fragment_contact
+    override fun init() {
+        super.init()
+        header_title.text = getString(R.string.tab_contact_string)
+        add.visibility = View.VISIBLE
+        //下拉刷新状态
+        swipeRefreshLayout.apply {
+            setColorSchemeResources(R.color.blue_light)
+            isRefreshing = true
+        }
+    }
 }
