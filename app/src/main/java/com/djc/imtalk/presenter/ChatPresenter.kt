ceed8a7ebc0e45ec133f0e97c1cfa69594e37eab
DJC <37604230+Dboy233@users.noreply.github.com>
2019-04-11 18:50:50 +0800

    聊天界面 presenter和view层完成

---
+++ app/src/main/java/com/djc/imtalk/presenter/ChatPresenter.kt
@@ -0,0 +1,19 @@
+package com.djc.imtalk.presenter
+
+import com.djc.imtalk.contract.ChatContract
+
+/**
+ *@author ： created by dujiangchuan
+ * 时间    ：2019/4/11 18
+ * 邮箱    ：894230813@qq.com
+ */
+class ChatPresenter(val view: ChatContract.View) : ChatContract.Presenter {
+
+    override fun sendMessage(contact: String, message: String) {
+
+
+    }
+
+
+
+}
--- app/src/main/java/com/djc/imtalk/ui/activity/ChatActivity.kt
+++ app/src/main/java/com/djc/imtalk/ui/activity/ChatActivity.kt
@@ -4,17 +4,20 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
 import com.djc.imtalk.R
+import com.djc.imtalk.contract.ChatContract
+import com.djc.imtalk.presenter.ChatPresenter
 import kotlinx.android.synthetic.main.activity_chat.*
 import kotlinx.android.synthetic.main.header.*
+import org.jetbrains.anko.toast
 
 /**
  *@author ： created by dujiangchuan
  * 时间    ：2019/4/10 20
  * 邮箱    ：894230813@qq.com
  */
-class ChatActivity : BaseActivity() {
-
-
+class ChatActivity : BaseActivity(), ChatContract.View {
+    val presenter = ChatPresenter(this)
+    lateinit var username: String
     override fun init() {
         initHead()
         initEditText()
@@ -27,13 +30,44 @@ class ChatActivity : BaseActivity() {
                 //输入框有文字时 可以发送
                 btn_sent.isEnabled = !s.isNullOrEmpty()
             }
+
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
             }
         })
+        ed_sent.setOnClickListener {
+            send()
+        }
+        ed_sent.setOnEditorActionListener { _, _, _ ->
+            send()
+            true
+        }
+    }
 
+    //发送信息
+    private fun send() {
+        hideSoftKeyboard()
+        val messageString = ed_sent.text.toString()
+        presenter.sendMessage(username, messageString)
+
+    }
+
+    override fun onStartSendMessage() {
+        //通知recyclerview刷新列表
+        recyclerView_chat.adapter?.notifyDataSetChanged()
+    }
+
+    override fun onSendMessageSuccess() {
+        recyclerView_chat.adapter?.notifyDataSetChanged()
+        toast("发送成功")
+        ed_sent.text.clear()
+    }
+
+    override fun onSendMessageFailed() {
+        toast("发送失败")
+        recyclerView_chat.adapter?.notifyDataSetChanged()
     }
 
     private fun initHead() {
@@ -41,7 +75,7 @@ class ChatActivity : BaseActivity() {
         back.setOnClickListener { finish() }
 
         //获取聊天的用户
-        val username = intent.getStringExtra("userName")
+        username = intent.getStringExtra("userName")
         val titleString = String.format("%s", username)
         header_title.text = username
 
