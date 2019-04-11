672cceae27f1b38e12e0779fdb80195ee102e530
DJC <37604230+Dboy233@users.noreply.github.com>
2019-04-11 14:30:43 +0800

    添加好友界面 View层实现

--- app/src/main/java/com/djc/imtalk/ui/activity/AddFriendActivity.kt
+++ app/src/main/java/com/djc/imtalk/ui/activity/AddFriendActivity.kt
@@ -3,16 +3,19 @@ package com.djc.imtalk.ui.activity
 import android.support.v7.widget.LinearLayoutManager
 import com.djc.imtalk.R
 import com.djc.imtalk.adapter.AddFriendListAdapter
+import com.djc.imtalk.contract.AddFriendContract
 import kotlinx.android.synthetic.main.acticity_add_friend.*
 import kotlinx.android.synthetic.main.header.*
+import org.jetbrains.anko.toast
 
 /**
  *@author ： created by dujiangchuan
  * 时间    ：2019/4/11 14
  * 邮箱    ：894230813@qq.com
  */
-class AddFriendActivity : BaseActivity() {
+class AddFriendActivity : BaseActivity(), AddFriendContract.View {
     override fun getLayoutResId(): Int = R.layout.acticity_add_friend
+
     override fun init() {
         super.init()
         header_title.text = getString(R.string.add_friend)
@@ -23,6 +26,17 @@ class AddFriendActivity : BaseActivity() {
             adapter = AddFriendListAdapter(context)
         }
 
+    }
+
+    override fun onSearchSuccess() {
+        dissMissProgress()
+        toast("搜索成功")
+        recyclerView_add.adapter?.notifyDataSetChanged()
+    }
+
+    override fun onSearchFailed() {
+        dissMissProgress()
+        toast("搜索失败")
     }
 
 }
