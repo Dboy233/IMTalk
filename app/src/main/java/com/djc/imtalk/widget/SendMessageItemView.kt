91bcb2b6b6c0a27ebdfe59b6a9c2c630157a1bd5
DJC <37604230+Dboy233@users.noreply.github.com>
2019-04-11 18:39:10 +0800

    聊天界面 收发布局完成

---
+++ app/src/main/java/com/djc/imtalk/widget/SendMessageItemView.kt
@@ -0,0 +1,18 @@
+package com.djc.imtalk.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.RelativeLayout
+import com.djc.imtalk.R
+
+/**
+ *@author ： created by dujiangchuan
+ * 时间    ：2019/4/11 17
+ * 邮箱    ：894230813@qq.com
+ */
+class SendMessageItemView(context: Context?, attrs: AttributeSet? = null) : RelativeLayout(context, attrs) {
+    init {
+        View.inflate(context, R.layout.view_send_message_item, this)
+    }
+}
