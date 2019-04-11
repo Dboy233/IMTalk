ca36c1b6b9555998b02b649ae664f9683b6bd419
DJC <37604230+Dboy233@users.noreply.github.com>
2019-04-11 18:56:08 +0800

    聊天界面 model层完成

--- app/src/main/java/com/djc/imtalk/presenter/ChatPresenter.kt
+++ app/src/main/java/com/djc/imtalk/presenter/ChatPresenter.kt
@@ -1,6 +1,9 @@
 package com.djc.imtalk.presenter
 
+import com.djc.imtalk.adapter.EMCallBackAdapter
 import com.djc.imtalk.contract.ChatContract
+import com.hyphenate.chat.EMClient
+import com.hyphenate.chat.EMMessage
 
 /**
  *@author ： created by dujiangchuan
@@ -9,11 +12,33 @@ import com.djc.imtalk.contract.ChatContract
  */
 class ChatPresenter(val view: ChatContract.View) : ChatContract.Presenter {
 
+    val messages = mutableListOf<EMMessage>()
+
+
     override fun sendMessage(contact: String, message: String) {
+        //创建一条消息
+        val emMessage = EMMessage.createTxtSendMessage(message, contact)
+        emMessage.setMessageStatusCallback(object : EMCallBackAdapter() {
+            override fun onSuccess() {
+                uiThread { view.onSendMessageSuccess() }
+            }
+
+            override fun onError(p0: Int, p1: String?) {
+                uiThread { view.onSendMessageFailed() }
+
+            }
+        })
+
+
+        //添加到消息列队
+        messages.add(emMessage)
+        //通知view层开始发送
+        view.onStartSendMessage()
+        //调用api发送信息
+        EMClient.getInstance().chatManager().sendMessage(emMessage)
 
 
     }
-
 
 
 }
