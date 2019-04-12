e481c0ac61f304a4f3816068e50d1b231153d414
DJC <37604230+Dboy233@users.noreply.github.com>
2019-04-12 11:21:02 +0800

    会话界面 收到消息刷新会话列表

--- app/src/main/java/com/djc/imtalk/ui/fragment/ConversationFragment.kt
+++ app/src/main/java/com/djc/imtalk/ui/fragment/ConversationFragment.kt
@@ -3,8 +3,10 @@ package com.djc.imtalk.ui.fragment
 import android.support.v7.widget.LinearLayoutManager
 import com.djc.imtalk.R
 import com.djc.imtalk.adapter.ConversationListAdapter
+import com.djc.imtalk.adapter.EMMessageListenerAdapter
 import com.hyphenate.chat.EMClient
 import com.hyphenate.chat.EMConversation
+import com.hyphenate.chat.EMMessage
 import kotlinx.android.synthetic.main.fragment_conversation.*
 import kotlinx.android.synthetic.main.header.*
 import org.jetbrains.anko.doAsync
@@ -28,17 +30,30 @@ class ConversationFragment : BaseFragment() {
             layoutManager = LinearLayoutManager(context)
             adapter = ConversationListAdapter(context, conversations)
         }
+        EMClient.getInstance().chatManager().addMessageListener(messageListAdapter)
         loadConversations()
     }
 
     private fun loadConversations() {
-        conversations.clear()
         doAsync {
+            //清空会话列表
+            conversations.clear()
             val allConversations = EMClient.getInstance().chatManager().allConversations
             conversations.addAll(allConversations.values)
             uiThread {
                 recyclerView_conversation.adapter?.notifyDataSetChanged()
             }
         }
+    }
+
+    val messageListAdapter = object : EMMessageListenerAdapter() {
+        override fun onMessageReceived(p0: MutableList<EMMessage>?) {
+            loadConversations()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        EMClient.getInstance().chatManager().removeMessageListener(messageListAdapter)
     }
 }
